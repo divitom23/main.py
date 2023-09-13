9feda8a83cd49c96767511d60d70f46f280dde08
divitom23 <143746488+divitom23@users.noreply.github.com>
2023-09-13 11:55:32 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hi, good mornin")
+#cree par max divito
+#cree en 2023
+#projet tp 1 pour calculer le nombre de mots dans une phrase
+
+
+
+
+
+#je defini la fonction qui calcule le nombre de mots
+def motNombre(NbrMot):
+    return len(NbrMot.split(" "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
